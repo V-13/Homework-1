eaac6424ac8f9d5db6625b55e6ea070a47bbf08e
shaluks97 <46239857+shaluks97@users.noreply.github.com>
2019-08-13 23:28:55 +0530

    homework

---
+++ node.js/calculator.js
@@ -0,0 +1,11 @@
+const op = require('./operation')
+var x=12;
+var y=4;
+var z=op.add(x,y);
+var a=op.mul(x,y);
+var b=op.diff(x,y);
+var d=op.div(x,y);
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(d);
---
+++ node.js/operation.js
@@ -0,0 +1,16 @@
+module.exports.add=(a,b)=>{
+    var c=a+b;
+    return c;
+}
+module.exports.mul=(a,b)=>{
+    var c=a*b;
+    return c;
+}
+module.exports.diff=(a,b)=>{
+    var c=a-b;
+    return c;
+}
+module.exports.div=(a,b)=>{
+    var c=a/b;
+    return c;
+}
